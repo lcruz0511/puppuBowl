97630cf7501508d233da0f51aa89db783f225259
Dillan May <dillanmay_6@yahoo.com>
2022-10-13 13:46:52 -0500

    dillanNew

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Navbar, Puppies } from "./";
 
 const Main = () => {
+  const[allPuppies, setAllPuppies]=useState([])
+    
+    
+  async function fetchAllPuppies() {
+    try {
+      const response = await fetch(
+        "https://fsa-puppy-bowl.herokuapp.com/api/2209-FTB-ET-WEB-FT/players"
+      );
+      const result = await response.json();
+      const puppies = result.data.players
+      setAllPuppies(puppies)
+      ;
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+
+  useEffect(() => {
+    fetchAllPuppies();
+  }, []);
+  // console.log(allPuppies)
+
   return (
     <div id="main">
       <Navbar />
-      <Puppies />
+      <Puppies allPuppies={allPuppies}/>
     </div>
   );
 };
