e43fe3775f7ec22113f374f5e34d2b3da295014a
Leonardo <cruzleonardo831@gmail.com>
2022-10-14 13:59:08 -0500

    update Leo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const Navbar = () => {
   return (
     <div id="navbar">
-      <h2>Puppy Bowl</h2>
+      {/* <h2>Puppy Bowl</h2> */}
+
+      <div className = "navName">
+      <div className = "nameSearch">Name: </div>
+
+
+      {/* <form class="search-form" action="/search">
+          <input class= "search-bar" type="text" name="search-term" placeholder="search">
+          <button type="submit"><i class="material-icons">search</i></button>
+        </form> */}
+
+      <div className = "breedSearch">Breed: </div>
+      <button className="submitBtn">Submit</button>
+      </div>
     </div>
   );
 };
--- src/components/SinglePuppy.jsx
+++ src/components/SinglePuppy.jsx
@@ -7,10 +7,12 @@ const SinglePuppy = (props) => {
     <div className="puppy">
       <div className="headerName">
         <div className="name">{puppy.name}</div>
-        <div className="id">{puppy.id}</div>
+        <div className="id">#{puppy.id}</div>
       </div>
       <img src ={puppy.imageUrl} className ="img" />
       <button className="detailBtn">See Details</button>
+        {/* <Link to={`${puppy.id}`}>See Details</Link> */}
+        {/* <Link to= "/puppies">See Details</Link> */}
       <button className="deleteBtn">Delete From Roster</button>
     </div>
   );
